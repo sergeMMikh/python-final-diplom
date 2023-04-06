1cbc0e2a8fc7e5d659e9d1adf9504a4eea39ca0c
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-04-06 15:34:38 +0100

    Revert "improved: send_email.py"
    This reverts commit 2228061b01d8c4ee8d4e5508c0e09cc2fe1cd035.

--- orders/send_email.py
+++ orders/send_email.py
@@ -3,23 +3,26 @@ from django.core.mail import EmailMessage
 from rest_framework.authtoken.models import Token
 
 
-def send_email_4_verification(current_site: str,
-                              user_email: str,
-                              token: str) -> None:
+def send_email_4_verification(request, user):
+    current_site = get_current_site(request)
+    token, _ = Token.objects.get_or_create(user=user)
     message = f"Please follow this link to confirm your password: \n " \
-              f"http://{current_site}/api/v1/user/verify_email/" \
-              f"{token}"
-    send_email.delay(title='Verify email',
-                     message=message,
-                     from_='django_app',
-                     to=user_email)
+              f"http://{current_site.domain}/api/v1/user/verify_email/{token}"
+    email = EmailMessage(
+        'Verify email',
+        message,
+        to=[user.email],
+    )
+    email.send()
 
 
-def send_email_4_reset_passw(user_email: str,
-                             token: str) -> None:
+def send_email_4_reset_passw(user, token):
+    token, _ = Token.objects.get_or_create(user=user)
     message = f"Please use this token for you request : \n " \
               f"{token}"
-    send_email.delay(title='reset_password',
-                     message=message,
-                     from_='django_app',
-                     to=user_email)
+    email = EmailMessage(
+        'reset_password',
+        message,
+        to=[user.email],
+    )
+    email.send()
