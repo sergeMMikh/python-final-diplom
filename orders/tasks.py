2cd806e2a3e352a192daec479bc907ccd3bd3ec9
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-04-06 15:36:00 +0100

    updated: Procfile

--- orders/tasks.py
+++ orders/tasks.py
@@ -30,23 +30,3 @@ def send_email_4_reset_passw(user_email, token):
         to=[user_email],
     )
     email.send()
-
-#
-# @shared_task
-# def add(x, y):
-#     # Celery recognizes this as the `movies.tasks.add` task
-#     # the name is purposefully omitted here.
-#     return x + y
-#
-#
-# @shared_task(name="multiply_two_numbers")
-# def mul(x, y):
-#     # Celery recognizes this as the `multiple_two_numbers` task
-#     total = x * (y * random.randint(3, 100))
-#     return total
-#
-#
-# @shared_task(name="sum_list_numbers")
-# def xsum(numbers):
-#     # Celery recognizes this as the `sum_list_numbers` task
-#     return sum(numbers)
